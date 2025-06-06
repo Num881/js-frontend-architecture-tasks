259f33a01e18d28bca565074b7502b040300688a
Num881 <ibabenko894@gmail.com>
2025-06-06 14:16:30 +0300

    Add files via upload

--- 1/src/application.js
+++ 1/src/application.js
@@ -1,3 +1,38 @@
 // BEGIN
+export default () => {
+    const calcForm = document.querySelector('form');
+    const numberInput = calcForm.querySelector('input[name="number"]');
+    const display = document.getElementById('result');
+    const resetBtn = calcForm.querySelector('button[type="button"]');
 
+    let totalSum = 0;
+
+    function refreshDisplay() {
+        display.textContent = totalSum;
+    }
+
+    function resetInput() {
+        calcForm.reset();
+        numberInput.focus();
+    }
+
+    calcForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        const enteredValue = parseInt(numberInput.value, 10);
+        if (!isNaN(enteredValue)) {
+            totalSum += enteredValue;
+            refreshDisplay();
+        }
+        resetInput();
+    });
+
+    resetBtn.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        totalSum = 0;
+        refreshDisplay();
+        resetInput();
+    });
+
+    numberInput.focus();
+};
 // END
